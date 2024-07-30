521a3b0f96a92028e65ed9b3527bc28e35b84143
Mushabberul Islam <dev.sabbirmia@gmail.com>
2024-07-30 08:40:12 +0600

    install sweetalert2 and configar

--- inventory-frontend/src/main.js
+++ inventory-frontend/src/main.js
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import VueSweetalert2 from 'vue-sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
+
 import App from './App.vue'
 import router from './router'
 
@@ -10,5 +13,9 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 app.use(VeeValidatePlugin);
+app.use(VueSweetalert2,{
+    confirmButtonColor: '#41b882',
+    cancelButtonColor: '#ff7674',
+  });
 
 app.mount('#app')
